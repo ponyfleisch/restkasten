76a705217c7adf5605ffdf7ae692f328f3539875
Claudio Mettler <github@ponyfleisch.ch>
2014-07-07 23:29:42 +0800

    error handling returns 403 now

--- app/controllers/rest_controller.rb
+++ app/controllers/rest_controller.rb
@@ -1,9 +1,15 @@
 class RestController < ApplicationController
   before_filter :set_default_response_format
   skip_before_action :verify_authenticity_token
+  rescue_from Exception, :with => :access_error
 
   private
   def set_default_response_format
     request.format = :json
   end
+
+  def access_error
+    render :nothing => true, :status => 403
+  end
+
 end
