78258c37f25956737b9200b4c4f307238379bc89
Claudio Mettler <github@ponyfleisch.ch>
2014-07-07 15:53:01 +0800

    simple users resource. jbuilder generated with rails generate jbuilder users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+  before_filter :set_default_response_format
+  skip_before_action :verify_authenticity_token
+
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render :show, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  def set_default_response_format
+    request.format = :json
+  end
+
+  def user_params
+    params.permit(:name, :password)
+  end
+
+
 end
